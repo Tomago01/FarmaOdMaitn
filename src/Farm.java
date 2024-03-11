f29036475cf4e41a70822f12e50f6f6d4d2832f8
Bures3 <Tom.bur.x123@gmail.com>
2024-03-11 08:57:47 +0100

    added method barn and constructor in Animal used in Sheep

--- src/Farm.java
+++ src/Farm.java
@@ -2,6 +2,7 @@ import java.util.ArrayList;
 
 public class Farm {
 private int sizeOfProperty = 100;
+ArrayList<Animal> insideBarn = new ArrayList();
 ArrayList<Flower> plantedPlants = new ArrayList();
 
 private ArrayList plantFlower(Flower f){
@@ -11,6 +12,18 @@ private ArrayList plantFlower(Flower f){
 private double waterPlant(Flower f){
     return f.chanceOfGrowth++;
 }
+
+private void barn(Animal a){
+    if (insideBarn.size()<=20) {
+        if (a.size.equals(Size.BIG) && insideBarn.size() <= 10) {
+            insideBarn.add(a);
+        } else if (a.size.equals(Size.SMALL) && insideBarn.size() <= 10) {
+            insideBarn.add(a);
+        }
+    }
+}
+
+
 
 
 
--- src/Sheep.java
+++ src/Sheep.java
@@ -1,8 +1,5 @@
 public class Sheep extends Animal{
-
-
-
-
-
-
+    public Sheep(String type, String name, Size size, String specialCare) {
+        super("dvds", "sdjvhkdjvds", Size.BIG, "true");
+    }
 }
