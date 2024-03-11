afc26311adadae053a0a3519a5645fe5ccaddc81
Bures3 <Tom.bur.x123@gmail.com>
2024-03-11 08:27:41 +0100

    added method plantFlower and waterPlant

--- src/Farm.java
+++ src/Farm.java
@@ -1,4 +1,18 @@
+import java.util.ArrayList;
+
 public class Farm {
+private int sizeOfProperty = 100;
+ArrayList<Flower> plantedPlants = new ArrayList();
+
+private ArrayList plantFlower(Flower f){
+    plantedPlants.add(f);
+    return plantedPlants;
+}
+private double waterPlant(Flower f){
+    return f.chanceOfGrowth++;
+}
+
+
 
 
 
--- src/Flower.java
+++ src/Flower.java
@@ -7,7 +7,6 @@ public abstract class Flower {
     protected double price;
     protected double neededArea;
     protected double chanceOfGrowth;
-    ArrayList<Flower> flowers = new ArrayList();
     public Flower(String name, double price, double neededArea, double chanceOfGrowth) {
         this.name = name;
         this.price = price;
@@ -47,10 +46,6 @@ public abstract class Flower {
         this.chanceOfGrowth = chanceOfGrowth;
     }
 
-
-    private void addFlower(Flower f){
-        flowers.add(f);
-    }
 
 
 
