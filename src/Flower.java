16db45f287e2d25557c2551169dcd64f92260d78
Bures3 <Tom.bur.x123@gmail.com>
2024-03-11 08:13:08 +0100

    added method addFlower

--- src/Flower.java
+++ src/Flower.java
@@ -1,9 +1,58 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public abstract class Flower {
 
     protected String name;
     protected double price;
     protected double neededArea;
     protected double chanceOfGrowth;
+    ArrayList<Flower> flowers = new ArrayList();
+    public Flower(String name, double price, double neededArea, double chanceOfGrowth) {
+        this.name = name;
+        this.price = price;
+        this.neededArea = neededArea;
+        this.chanceOfGrowth = chanceOfGrowth;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public double getNeededArea() {
+        return neededArea;
+    }
+
+    public void setNeededArea(double neededArea) {
+        this.neededArea = neededArea;
+    }
+
+    public double getChanceOfGrowth() {
+        return chanceOfGrowth;
+    }
+
+    public void setChanceOfGrowth(double chanceOfGrowth) {
+        this.chanceOfGrowth = chanceOfGrowth;
+    }
+
+
+    private void addFlower(Flower f){
+        flowers.add(f);
+    }
+
+
 
     @Override
     public String toString() {
